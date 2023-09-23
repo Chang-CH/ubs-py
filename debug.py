432ecef157113beffdbe01971b3d5c43f4a09fd6
Chang-CH <chang.ch4348@gmail.com>
2023-09-23 23:40:43 +0800

    ctf3

--- debug.py
+++ debug.py
@@ -1,42 +1,63 @@
-def max_earnings(lessons):
-    max_earnings = [0] * 13  # Initialize a list to store maximum earnings for each possible duration
-    selected_lessons = [[] for _ in range(13)]  # Initialize a list to store selected lessons for each duration
-
-    for lesson in lessons:
-        for duration in range(12, lesson["duration"] - 1, -1):
-            # Check if adding the current lesson improves earnings
-            if max_earnings[duration - lesson["duration"]] + lesson["potentialEarnings"] > max_earnings[duration]:
-                max_earnings[duration] = max_earnings[duration - lesson["duration"]] + lesson["potentialEarnings"]
-                selected_lessons[duration] = selected_lessons[duration - lesson["duration"]] + [lesson["lessonRequestId"]]
-
-    return max_earnings[12], selected_lessons[12]
-
 lessons = [
     {
         "lessonRequestId": "LR1",
         "duration": 1,
         "potentialEarnings": 100,
         "availableDays": ["monday", "wednesday"]
-    }, {
+    },
+    {
         "lessonRequestId": "LR2",
         "duration": 2,
-        "potentialEarnings": 800,
-        "availableDays": ["friday"]
-    }, {
+        "potentialEarnings": 50,
+        "availableDays": ["monday"]
+    },
+    {
         "lessonRequestId": "LR3",
-        "duration": 6,
-        "potentialEarnings": 800,
-        "availableDays": ["friday"]
-    }, {
-        "lessonRequestId": "LR4",
         "duration": 12,
         "potentialEarnings": 1000,
-        "availableDays": ["friday"]
+        "availableDays": ["wednesday"]
+    },
+    {
+        "lessonRequestId": "LR4",
+        "duration": 13,
+        "potentialEarnings": 10000,
+        "availableDays": ["sunday"]
     }
 ]
 
-total_earnings, selected_lessons = max_earnings(lessons)
-print("Maximum possible earnings:", total_earnings)
-print("Selected lessons:")
-for lesson_id in selected_lessons:
-    print(f"- {lesson_id}")
+# Create a list of available days and their hours
+available_hours = {
+    "monday": 12,
+    "tuesday": 12,
+    "wednesday": 12,
+    "thursday": 12,
+    "friday": 12,
+    "saturday": 12,
+    "sunday": 12
+}
+
+# Initialize a dictionary to store the maximum earnings for each day and remaining hours
+dp = {day: {hours: 0 for hours in range(available_hours[day] + 1)} for day in available_hours}
+
+# Fill the DP dictionary
+for lesson in lessons:
+    for day in lesson["availableDays"]:
+        for hours in range(available_hours[day], lesson["duration"] - 1, -1):
+            dp[day][hours] = max(dp[day][hours], dp[day][hours - lesson["duration"]] + lesson["potentialEarnings"])
+
+# Reconstruct the optimal solution
+optimal_lessons = []
+day = "sunday"  # Start with any day
+remaining_hours = available_hours[day]
+
+for lesson in reversed(lessons):
+    if remaining_hours >= lesson["duration"] and dp[day][remaining_hours] == dp[day][remaining_hours - lesson["duration"]] + lesson["potentialEarnings"]:
+        optimal_lessons.append(lesson)
+        day = [d for d in lesson["availableDays"] if d != day][0]
+        remaining_hours -= lesson["duration"]
+
+# Print the optimal lessons and their potential earnings
+total_earnings = sum(lesson["potentialEarnings"] for lesson in optimal_lessons)
+for lesson in optimal_lessons:
+    print(f"Lesson {lesson['lessonRequestId']} - Duration: {lesson['duration']} hours, Earnings: ${lesson['potentialEarnings']}")
+print(f"Total Earnings: ${total_earnings}")
