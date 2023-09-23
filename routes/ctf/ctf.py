b010e3092ae82708f04ca6eeec9dd0a026b6bbd3
Chang-CH <chang.ch4348@gmail.com>
2023-09-23 12:39:14 +0800

    CTF

--- routes/ctf/ctf.py
+++ routes/ctf/ctf.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, session, jsonify, request, make_response
 
+payload_crackme = Blueprint("payload_crackme", __name__)
 
-@greedymonkey.route("/payload_crackme", methods=["GET"])
+@payload_crackme.route("/payload_crackme", methods=["GET"])
 def getCommon():
     return send_file("payload_crackme")
